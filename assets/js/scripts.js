25e90ccd055bfe2f7aff5cdbd837cc687961875a
hisarkaya <hisarkaya@hotmail.com>
2017-05-26 09:28:41 +0100

    google map is added to contact page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,15 @@ function toggleMenu() {
         menu.className = 'main-menu'
     }
 }
+
+function initMap() {
+    var coordinate = { lat: 51.556154, lng: -0.074844 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: coordinate
+    });
+    var marker = new google.maps.Marker({
+        position: coordinate,
+        map: map
+    });
+}
